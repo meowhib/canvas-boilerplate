7c8c05a2f2c43ef63d474b68ebd294e107803de6
meowhib <thisisjusttestingpython@gmail.com>
2021-10-19 10:46:56 +0100

    Fixed some functions
    Added min max to randInt.
    Changed default background to black.
    Added radius parameter to drawDisc.

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ window.addEventListener("mousemove", function(e){
 })
 
 window.addEventListener("keydown", function(e){
-    // Keycodes (32 space) (13 enter) (38 UP 37 LEFT 39 RIGHT 40 DOWN)
+    // Keycodes (32 space) (13 enter) (38 UP) (37 LEFT) (39 RIGHT) (40 DOWN)
 })
 
 // Functions
@@ -35,7 +35,7 @@ function randBool(){
     return Math.random() < 0.5 ? true : false;
 }
 
-function randInt(){
+function randInt(min, max){
     return Math.floor(Math.random() * (max - min) ) + min;
 }
 
@@ -43,14 +43,16 @@ function clearScr(){
     ctx.clearRect(0, 0, width, height);
 }
 
-function drawDisc(x, y){
+function drawDisc(x, y, r){
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, PI * 2, true);
+    ctx.fillStyle = "#FFFFFF";
+    ctx.arc(x, y, r, 0, PI * 2, true);
     ctx.fill();
 }
 
 function drawLine(pt0, pt1){
     ctx.beginPath();
+    ctx.strokeStyle = "#FFFFFF";
     ctx.moveTo(pt0.x, pt0.y);
     ctx.lineTo(pt1.x, pt1.y);
     ctx.stroke();
